1f50205c4290a70da5d13d29f25239f1e843e5a1
Blessing Okwong <okwongnorma@gmail.com>
2023-12-07 17:25:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,69 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
-import './index.css';
+// import './index.css';
+
+// import App from './App.jsx';
+// import Contact from './pages/Contact.jsx';
+// import SignIn from './pages/SignIn.jsx';
+// import SignUp from './pages/SignUp.jsx';
+
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: 'contact',
+//     element: <Contact />,
+//   },
+//   {
+//     path: 'signin',
+//     element: <SignIn />,
+//   },
+//   {
+//     path: 'signup',
+//     element: <SignUp />,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   //   {/* Wrap your entire application with RouterProvider */}
+//   //   <RouterProvider router={router}>
+//   //     {/* <App /> */}
+//   //   </RouterProvider>
+//   // </React.StrictMode>,
+//   <RouterProvider router={router} />
+
+// );
+
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 import App from './App.jsx';
 import Contact from './pages/Contact.jsx';
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
   {
     path: 'contact',
     element: <Contact />,
   },
-  {
-    path: 'signin',
-    element: <SignIn />,
-  },
-  {
-    path: 'signup',
-    element: <SignUp />,
-  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* Wrap your entire application with RouterProvider */}
-    <RouterProvider router={router}>
-      {/* <App /> */}
-    </RouterProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
-
-
-// import * as React from "react";
-// import { createRoot } from "react-dom/client";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Route,
-//   Link,
-// } from "react-router-dom";
-
-// import App from './App.jsx';
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//   {
-//     path: "about",
-//     element: <div>About</div>,
-//   },
-// ]);
-
-// createRoot(document.getElementById("root")).render(
-//   <RouterProvider router={router} />
-// );
